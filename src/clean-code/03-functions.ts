1b1ad3ba5e5f5f95bd305d6dfab9d1256f50101f
FavoMeister <93888759+FavoMeister@users.noreply.github.com>
2025-08-31 17:59:08 -0400

    Functions exercices

---
+++ src/clean-code/03-functions.ts
@@ -0,0 +1,45 @@
+(() => {
+
+    // función para obtener información de una película por Id
+    function getMovieById( id: number ) {
+        console.log({ id });
+    }
+
+    // función para obtener información de los actores de una película - Actors o Cast // id = movieId getMovieCast
+    function getMovieActorsById( id: number ) {
+        console.log({ id });
+    }
+
+    // funcion para obtener el bio del actor por el id
+    function getActorBioById( ActorId: number ) {
+        console.log({ ActorId });
+    }
+    
+    // Crear una película
+    interface movie {
+        title: string;
+        description: string;
+        rating: number;
+        cast: string[];
+    }
+    function createMovie({title, description, rating, cast}: movie ) {
+        console.log({ title, description, rating, cast });
+    }
+
+    // Crea un nuevo actor
+    function createActor( fullName: string, birthDate: Date ): boolean {
+        
+        // tarea asincrona para verificar nombre
+        // ..
+        // ..
+        if ( fullName === 'Yolo' ) return false;
+
+        console.log('Crear actor', birthDate);
+        return true;        
+
+    }
+
+    
+
+
+})();
