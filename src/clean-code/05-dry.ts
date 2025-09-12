c04553953ddc11fc520477a72a85018ee0d9fc69
FavoMeister <93888759+FavoMeister@users.noreply.github.com>
2025-09-11 23:06:32 -0400

    Applying DRY

--- src/clean-code/05-dry.ts
+++ src/clean-code/05-dry.ts
@@ -7,11 +7,33 @@ class Product {
         public size: Size = ''
     ){}
 
+    isProductReady():boolean {
+        for (const key in this) {
+            switch (typeof this[key]) {
+                case 'string':
+                    if ((<string><unknown>this[key]).length <= 0)  throw Error(`${ key } is empty`)
+                    break;
+                case 'number':
+                    if ((<number><unknown>this[key]) <= 0)  throw Error(`${ key } is zero`)
+                    break;
+
+                default:
+                    throw Error(`${ typeof this[key] } is not valid`)
+                    break;
+            }   
+        }
+        return true;
+    }
+
     toString() {
         // no DRY
-        if( this.name.length <= 0) throw Error ('name is empty');
+        /* if( this.name.length <= 0) throw Error ('name is empty');
         if( this.price <= 0) throw Error ('price is zero');
-        if( this.size.length <= 0) throw Error ('size is empty');
+        if( this.size.length <= 0) throw Error ('size is empty'); */
+        //const strinProperties = ['name', 'size']
+
+        if ( !this.isProductReady ) 
+            return;
 
         return `${ this.name } (${ this.price }), ${this.size} `
     }
