46e19214f432166b061ae08367b102ef16345e48
FavoMeister <93888759+FavoMeister@users.noreply.github.com>
2025-11-03 16:33:59 -0400

    principio de resposabilidad única

--- src/clean-code/06-classes-a.ts
+++ src/clean-code/06-classes-a.ts
@@ -17,6 +17,48 @@
         }
     }
 
+    class User extends Person {
+
+        public lastAccess: Date;
+        constructor(
+            public email: string,
+            public role: string,
+            //private lastAccess: Date,
+            name: string,
+            gender: Gender,
+            birthdate: Date,
+        ){
+            super( name, gender, birthdate) // for call the fathers constructor
+            this.lastAccess = new Date();
+        }
+    }
+
+    class UserSettings extends  User {
+        constructor(
+            public workingDirectory: string,
+            public lastOpenFolder: string,
+            email: string,
+            role: string,
+            name: string,
+            gender: Gender,
+            birthdate: Date
+        ){
+            super(email, role, name, gender, birthdate)
+        }
+    }
+
+    const userSettigs = new UserSettings(
+        '/usr/home',
+        '/home',
+        'fmollinedo@gmail.com',
+        'Admin',
+        'Favio',
+        'M',
+        new Date('1990-12-12')
+    )
+    console.log({userSettigs}); //no cumple el pirncipio de responsabilidad única, es muy dificil de mantener el código
+    
+
     const newPerson = new Person('German', 'M', new Date('1975-12-22'));
     console.log({ newPerson });
 
