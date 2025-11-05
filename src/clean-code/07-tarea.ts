a761d0209a55f74a47ad9cb9938672b8bb005fcb
FavoMeister <93888759+FavoMeister@users.noreply.github.com>
2025-11-05 00:14:28 -0400

    Homework about PRU and Composition

---
+++ src/clean-code/07-tarea.ts
@@ -0,0 +1,69 @@
+(()=>{
+
+    //* Aplicar el principio de responsabilidad única
+    //* Priorizar la composición frente a la herencia
+
+    type HtmlType = 'input'|'select'|'textarea'|'radio';
+   
+    class HtmlElement {
+        constructor(
+            public id: string,
+            public type: HtmlType,
+        ) {}
+    }
+
+    /* class InputAttributes extends HtmlElement {
+        constructor(
+            public value: string,
+            public placeholder: string,
+            id: string,
+        ) {
+            super(id, 'input');
+        }
+    } */
+    class InputAttributes {
+        constructor(
+            public value: string,
+            public placeholder: string,
+        ) {}
+    }
+
+    /* class InputEvents extends InputAttributes {
+        constructor( value: string, placeholder: string, id: string ) {
+            super( value, placeholder, id );
+        }
+
+        setFocus() {};
+        getValue() {};
+        isActive() {};
+        removeValue() {};
+    } */
+
+    class InputEvents {
+        constructor() {}
+
+        setFocus() {};
+        getValue() {};
+        isActive() {};
+        removeValue() {};
+    }
+
+
+    //? Idea para la nueva clase InputElement
+    class InputElement {
+        public htmlElement: HtmlElement;
+        public inputAttributes: InputAttributes;
+        public events: InputEvents;
+
+        constructor(value: string, placeholder: string, id: string){
+            this.htmlElement = new HtmlElement(id, 'input');
+            this.inputAttributes = new InputAttributes(value, placeholder);
+            this.events = new InputEvents();
+        }
+    }
+
+    const nameField = new InputElement('Favio', 'Enter first name', 'txtName');
+
+    console.log({ nameField });
+
+})()
